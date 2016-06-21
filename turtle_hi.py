f7949793eb4868e694a25d18de58465fe8697cac
Ben Silbernagel <bensilbernagel@gmail.com>
2016-06-21 05:47:22 -0700

    turtle drawing 'hi'

---
+++ turtle_hi.py
@@ -0,0 +1,57 @@
+# file name: turtle_hi.py
+# author: Ben Silbernagel
+# created: 4/14/2016
+# purpose: turtle draws the word "Hi"
+
+import turtle
+
+turtle.shape("turtle")
+
+# creates the H
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(25)
+turtle.left(180)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+turtle.left(90)
+
+# creates the i
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(65)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(65)
+turtle.left(90)
+
+turtle.shape("turtle")
+
+turtle.exitonclick()
